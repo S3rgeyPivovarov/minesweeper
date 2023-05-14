8b6b9d78428d83098667ff853590812522f5ab0d
S3rgeyPivovarov <111754932+S3rgeyPivovarov@users.noreply.github.com>
2023-05-14 19:50:40 +0300

    feat: add fu create matrix and DOM, add CSS styles

--- minesweeper/script.js
+++ minesweeper/script.js
@@ -1,17 +1,56 @@
-let sizeMatrix = 16;
+let sizeMatrix = 15;
+let numberOfMines = 2;
 const matrix = [];
-const cell = { isMine: false, flag: false, open: false };
 
-for (let i = 0; i < sizeMatrix; i++) {
-  matrix.push([]);
-  for (let j = 0; j < sizeMatrix; j++) {
-    matrix[i].push([Object.assign({}, cell)]);
+const body = document.querySelector(".body");
+
+body.innerHTML = "<div class='container'><div class='minesField'></div></div>";
+
+const minesField = document.querySelector(".minesField");
+
+function newDomField() {
+  minesField.innerHTML = "";
+  let numberOfCell = sizeMatrix ** 2;
+  for (let i = 0; i < numberOfCell; i++) {
+    minesField.innerHTML += "<div class='cell'>aa</div>";
   }
+  minesField.style.width = sizeMatrix * 40 + "px";
+  minesField.style.height = sizeMatrix * 40 + "px";
 }
 
-let numberOfMines = 10;
-let n = 256
-const number = [];
-const numberOfCellsWithMines = [...Array(numberOfMines).keys()];
-console.log(number);
-while (number.lenth < n)
+newDomField();
+function NewMatrix() {
+  for (let i = 0; i < sizeMatrix; i++) {
+    matrix.push([]);
+    for (let j = 0; j < sizeMatrix; j++) {
+      matrix[i].push({ isMine: false, flag: false, open: false });
+    }
+  }
+
+  function fuRandomNumber() {
+    return Math.floor(Math.random() * sizeMatrix ** 2 + 0.99999999);
+  }
+  let randomNumber, x;
+  const arrayMines = [...Array(numberOfMines)];
+  arrayMines.forEach((_, i) => {
+    x = true;
+    while (x) {
+      x = false;
+      randomNumber = fuRandomNumber();
+      arrayMines.forEach((element) => {
+        if (element === randomNumber) {
+          x = true;
+        }
+      });
+    }
+    arrayMines[i] = randomNumber;
+  });
+
+  arrayMines.forEach((element) => {
+    i = Math.trunc(element / sizeMatrix - 0.1);
+    j = element - i * sizeMatrix - 1;
+    matrix[i][j].isMine = true;
+  });
+}
+
+NewMatrix();
