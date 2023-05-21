00233f3a759ecbf4a79bffd85fe6fe110e03c047
S3rgeyPivovarov <111754932+S3rgeyPivovarov@users.noreply.github.com>
2023-05-21 22:16:03 +0300

    feat: add save

--- minesweeper/script.js
+++ minesweeper/script.js
@@ -1,17 +1,18 @@
 let sizeMatrix = 10;
-let numberOfMines = 10;
+let numberOfMines = 1;
 
 let matrixIsCreate = false;
 
-let isBlockedCell = false;
-
 let matrix = [];
-savedMatrix = JSON.parse(localStorage.getItem("matrix"));
-console.log(savedMatrix);
+
+let isBlocked = false;
 
 const body = document.querySelector(".body");
 
-body.innerHTML = "<div class='container'><div class='minesField'></div></div>";
+body.innerHTML =
+  "<div class='container'><div class='menu'>New game</div><div class='minesField'></div></div>";
+
+const minesField = document.querySelector(".minesField");
 
 document
   .querySelector(".container")
@@ -19,9 +20,7 @@ document
     event.preventDefault();
   });
 
-const minesField = document.querySelector(".minesField");
-
-function createDom() {
+function createHTMLField() {
   minesField.innerHTML = "";
   for (let i = 0; i < sizeMatrix ** 2; i++) {
     minesField.innerHTML += "<div class='cell'></div>";
@@ -29,94 +28,108 @@ function createDom() {
     minesField.style.height = sizeMatrix * 40 + "px";
   }
 }
-createDom();
+createHTMLField();
 let cell = document.querySelectorAll(".cell");
 
-//ghjdthbnm
-if (!savedMatrix === null) {
-  matrix = sizeMatrix;
-  refactorDom();
-}
+let newGame = document.querySelector(".menu");
 
-function newMatrix(firstCell) {
-  for (let i = 0; i < sizeMatrix; i++) {
-    matrix.push([]);
-    for (let j = 0; j < sizeMatrix; j++) {
-      matrix[i].push({
-        isNotMine: true,
-        isFlag: false,
-        isOpen: false,
-        isOpenChecked: false,
-      });
-    }
-  }
-  function fuRandomNumber() {
-    return Math.trunc(Math.random() * sizeMatrix ** 2 - 0.0001);
-  }
-  let randomNumber, x;
-  const arrayMines = [...Array(numberOfMines)];
-  arrayMines.forEach((_, i) => {
-    x = true;
-    while (x) {
-      x = false;
-      randomNumber = fuRandomNumber();
-      arrayMines.forEach((element) => {
-        if (element === randomNumber || randomNumber == firstCell) {
-          x = true;
-        }
-      });
-    }
-    arrayMines[i] = randomNumber;
-  });
+newGame.addEventListener("click", (event) => {
+  localStorage.setItem("matrix", JSON.stringify(null));
+  isBlocked = false;
+  matrix = [];
+  createMatrix(99999);
+  refactorField();
+});
 
-  arrayMines.forEach((element) => {
-    let i = Math.trunc(element / sizeMatrix);
-    let j = element % sizeMatrix;
-    matrix[i][j].isNotMine = false;
-  });
-  matrix.forEach((_, i) => {
-    matrix[i].forEach((_, j) => {
-      if (matrix[i][j].isNotMine) {
-        let n = 0;
-        if (i > 0 && !matrix[i - 1][j].isNotMine) {
-          n++;
-        }
-        if (i > 0 && j < sizeMatrix - 1 && !matrix[i - 1][j + 1].isNotMine) {
-          n++;
-        }
-        if (j < sizeMatrix - 1 && !matrix[i][j + 1].isNotMine) {
-          n++;
-        }
-        if (
-          i < sizeMatrix - 1 &&
-          j < sizeMatrix - 1 &&
-          !matrix[i + 1][j + 1].isNotMine
-        ) {
-          n++;
-        }
-        if (i < sizeMatrix - 1 && !matrix[i + 1][j].isNotMine) {
-          n++;
-        }
-        if (i < sizeMatrix - 1 && j > 0 && !matrix[i + 1][j - 1].isNotMine) {
-          n++;
-        }
-        if (j > 0 && !matrix[i][j - 1].isNotMine) {
-          n++;
-        }
-        if (i > 0 && j > 0 && !matrix[i - 1][j - 1].isNotMine) {
-          n++;
-        }
-        if (n) {
-          matrix[i][j].isNotMine = n;
-        } else {
-          matrix[i][j].isNotMine = true;
-        }
+function createMatrix(firstCell) {
+  if (!!JSON.parse(localStorage.getItem("matrix"))) {
+    console.log(!!JSON.parse(localStorage.getItem("matrix")));
+    matrix = JSON.parse(localStorage.getItem("matrix"));
+    matrixIsCreate = true;
+    refactorField();
+  } else {
+    for (let i = 0; i < sizeMatrix; i++) {
+      matrix.push([]);
+      for (let j = 0; j < sizeMatrix; j++) {
+        matrix[i].push({
+          isNotMine: true,
+          isFlag: false,
+          isOpen: false,
+          isOpenChecked: false,
+        });
       }
+    }
+    function fuRandomNumber() {
+      return Math.trunc(Math.random() * sizeMatrix ** 2 - 0.0001);
+    }
+    let randomNumber, x;
+    const arrayMines = [];
+    for (let n = 0; n < numberOfMines; n++) {
+      arrayMines.push(0);
+    }
+    arrayMines.forEach((_, i) => {
+      x = true;
+      while (x) {
+        x = false;
+        randomNumber = fuRandomNumber();
+        arrayMines.forEach((element) => {
+          if (element === randomNumber || randomNumber == firstCell) {
+            x = true;
+          }
+        });
+      }
+      arrayMines[i] = randomNumber;
     });
-  });
+
+    arrayMines.forEach((element) => {
+      let i = Math.trunc(element / sizeMatrix);
+      let j = element % sizeMatrix;
+      matrix[i][j].isNotMine = false;
+    });
+    matrix.forEach((_, i) => {
+      matrix[i].forEach((_, j) => {
+        if (matrix[i][j].isNotMine) {
+          let n = 0;
+          if (i > 0 && !matrix[i - 1][j].isNotMine) {
+            n++;
+          }
+          if (i > 0 && j < sizeMatrix - 1 && !matrix[i - 1][j + 1].isNotMine) {
+            n++;
+          }
+          if (j < sizeMatrix - 1 && !matrix[i][j + 1].isNotMine) {
+            n++;
+          }
+          if (
+            i < sizeMatrix - 1 &&
+            j < sizeMatrix - 1 &&
+            !matrix[i + 1][j + 1].isNotMine
+          ) {
+            n++;
+          }
+          if (i < sizeMatrix - 1 && !matrix[i + 1][j].isNotMine) {
+            n++;
+          }
+          if (i < sizeMatrix - 1 && j > 0 && !matrix[i + 1][j - 1].isNotMine) {
+            n++;
+          }
+          if (j > 0 && !matrix[i][j - 1].isNotMine) {
+            n++;
+          }
+          if (i > 0 && j > 0 && !matrix[i - 1][j - 1].isNotMine) {
+            n++;
+          }
+          if (n) {
+            matrix[i][j].isNotMine = n;
+          } else {
+            matrix[i][j].isNotMine = true;
+          }
+        }
+      });
+    });
+  }
 }
 
-function refactorDom() {
+function refactorField() {
   let i, j;
   cell.forEach((element, k) => {
     i = Math.trunc(k / sizeMatrix);
@@ -133,6 +146,7 @@ function refactorDom() {
         element.innerHTML = matrix[i][j].isFlag ? "&check;" : "&#x2715;";
       }
     } else {
+      element.style.outline = "";
       if (matrix[i][j].isFlag) {
         element.innerHTML = "!";
       } else if (matrix[i][j].isNotMine === "winEnd") {
@@ -143,6 +157,8 @@ function refactorDom() {
     }
   });
 }
+
+createMatrix();
 
 function openingFreeCell() {
   let isNotFinishCheck = true;
@@ -221,9 +237,9 @@ function openingFreeCell() {
 
 cell.forEach((element, k) => {
   element.addEventListener("click", (event) => {
-    if (!isBlockedCell) {
+    if (!isBlocked) {
       if (!matrixIsCreate) {
-        newMatrix(k);
+        createMatrix(k);
         matrixIsCreate = true;
       }
 
@@ -233,24 +249,25 @@ cell.forEach((element, k) => {
         matrix[i][j].isOpen = true;
         if (!matrix[i][j].isNotMine) {
           gameOver();
+        } else {
+          openingFreeCell();
+          isWin();
+          localStorage.setItem("matrix", JSON.stringify(matrix));
         }
+        refactorField();
       }
-      openingFreeCell();
       isWin();
-      refactorDom();
-      localStorage.setItem("matrix", JSON.stringify(matrix));
     }
   });
 });
 
 cell.forEach((element, k) => {
   element.addEventListener("contextmenu", (event) => {
-    if (!isBlockedCell) {
+    if (!isBlocked) {
       let i = Math.trunc(k / sizeMatrix);
       let j = k % sizeMatrix;
       matrix[i][j].isFlag = !matrix[i][j].isFlag;
-      refactorDom();
-      localStorage.setItem("matrix", JSON.stringify(matrix));
+      refactorField();
     }
   });
 });
@@ -269,8 +286,9 @@ function isWin() {
         element.isNotMine = !element.isNotMine ? "winEnd" : element.isNotMine;
       });
     });
-
-    isBlockedCell = true;
+    isBlocked = true;
+    localStorage.setItem("matrix", JSON.stringify(null));
+    console.log("ss");
     // скрипт победы
   }
 }
@@ -283,6 +301,9 @@ function gameOver() {
       }
     })
   );
-  isBlockedCell = true;
+  isBlocked = true;
+  localStorage.setItem("matrix", JSON.stringify(null));
   //скрипт поражения
 }
+
+//при победе флаги не становятся галочкой
